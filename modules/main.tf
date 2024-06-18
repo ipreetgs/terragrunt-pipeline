a786082b65941c3e8dc1ae83a34f9035ce868c0b
Gurpreet <ipreet**@gmail.com>
2024-06-18 16:03:15 +0530

    pro

--- modules/main.tf
+++ modules/main.tf
@@ -1,3 +1,17 @@
+terraform {
+    backend "remote" {
+        hostname = "gurpreet1.jfrog.io"
+        organization = "tfstate"
+        token = get_env("JFROG_TOKEN")
+        workspaces {
+          name = "dev"
+            # prefix = "my-prefix-"
+        }
+    }
+}
+
+
+
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "edemotcchd12" # Change to your desired bucket name
 
